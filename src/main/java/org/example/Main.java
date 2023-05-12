9f24d0d3162f7dffa1c9f65bc39892cd9a5ea591
Hongse0 <hongseyoung981007@gmail.com>
2023-05-12 15:35:09 +0900

    start

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("게임 프로그래밍");
+        System.out.println("Hello world!");
+    }
+}
