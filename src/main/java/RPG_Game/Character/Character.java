3fde4409ea67af496d07f10c3c4c1b66a3f59615
Hongse0 <hongseyoung981007@gmail.com>
2023-05-16 15:21:41 +0900

    변수명 정리, 상점에서 무기 구매시 보유 잔액 줄어들고 캐릭터 공격력증가, 무기레벨 증가

--- src/main/java/RPG_Game/Character/Character.java
+++ src/main/java/RPG_Game/Character/Character.java
@@ -4,8 +4,8 @@ public class Character extends CommonChar {
     private String specialAbility;
 
 
-    public Character(String name, int hp, int mp, int dp, int attack, int experience, int level, int money) {
-        super(name, hp, mp, dp, attack, experience, level, money);
+    public Character(String name, int hp, int mp, int postionNum, int attack, int experience, int level, int money,int weaponLv) {
+        super(name, hp, mp, postionNum, attack, experience, level, money,weaponLv);
         super.displayInfo();
         this.specialAbility = specialAbility;
     }
@@ -17,8 +17,25 @@ public class Character extends CommonChar {
 
     public int getlevel(){return level;}
 
+    public int setweaponLv(int i){return this.weaponLv = i;}
+
+    public int getweaponLv(){return weaponLv;}
+
+
     public int getMoney() {
         return money;
     }
 
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int setMoney(int i) {
+        return this.money = i;
+    }
 }
