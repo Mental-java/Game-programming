23da1cb06512db3a2bae66e5c930aa9b55c2b781
YoungDong99 <114642786+YoungDong99@users.noreply.github.com>
2023-05-18 10:51:10 +0900

    Application 문자 입력 오류 예외 처리

--- src/main/java/RPG_Game/Run/Application.java
+++ src/main/java/RPG_Game/Run/Application.java
@@ -5,34 +5,37 @@ import RPG_Game.Character.Select;
 
 import RPG_Game.Town.TownMenu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        do{
+        do {
             Scanner sc = new Scanner(System.in);
             System.out.println("****************** The King - Joju ******************");
             System.out.println("1. 게임 시작");
-            System.out.println("2" + ". 게임 종료");
-            System.out.print("메뉴 선택 : ");
-            int menuNum = sc.nextInt();
-            System.out.println("");
+            System.out.println("2. 게임 종료");
+            System.out.print("메뉴 선택: ");
 
-            switch (menuNum){
-                case 1:
-                    Select s = new Select();
-                    Character character = s.makingCharacter();
-                    TownMenu townMenu = new TownMenu(character);
-                    townMenu.townMenu();
-                    break;
-                case 2:
-                    System.exit(0);
-                default:
-                    System.out.println("잘못된 번호를 입력하셨습니다.");
+            try {
+                int menuNum = sc.nextInt();
+
+                switch (menuNum) {
+                    case 1:
+                        System.out.println("case 1");
+                        break;
+                    case 2:
+                        return;
+                    default:
+                        System.out.println("잘못된 번호를 입력하셨습니다.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("[Error] 숫자를 입력해주세요.");
+                sc.nextLine(); // 버퍼 비우기
             }
-        }while (true);
+        } while (true);
 
     }
 
